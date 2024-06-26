ccc5eb3e2d1d3ba82384d8fc5ecabc33d00a172b
Gabriel Chia <tallspin_97@hotmail.com>
2024-06-26 20:24:02 +0800

    Added gitignore and loaded in textures with test

--- PumpItUpEngine/PumpItUpEngine/Source.cpp
+++ PumpItUpEngine/PumpItUpEngine/Source.cpp
@@ -1,14 +1,39 @@
 #include "SFML/Graphics.hpp"
+#include <iostream>
 
 int main() {
 	sf::RenderWindow window(sf::VideoMode(800, 800), "Pump it up");
 	sf::Event e;
+	int i = 0;
+
+	sf::Texture texture;
+	if (!texture.loadFromFile("../ArrowTextures/BASE 1x2.PNG")) {
+		std::cerr << "Failed to load image" << std::endl;
+		return -1;
+	}
+
+	// Create a sprite
+	sf::Sprite sprite(texture);
+	sprite.setPosition(200, 150);
 
 	while (window.isOpen()) {
 		while (window.pollEvent(e)) {
 			if (e.type == sf::Event::Closed)
 				window.close();
 		}
+
+		window.clear();
+		sf::CircleShape shape(50);
+		shape.setFillColor(sf::Color::Green);
+		shape.setPosition(i, i);
+
+		window.draw(shape);
+		window.draw(sprite);
+
+		// Display the content
+		window.display();
+		//i++;
+		std::cout << i << std::endl;
 	}
 	
 	return 0;
