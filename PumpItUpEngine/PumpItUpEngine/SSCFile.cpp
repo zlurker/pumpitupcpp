ace6f65f4329a3e2033e5dbc48ba8d629dca6890
Gabriel Chia <tallspin_97@hotmail.com>
2024-07-03 21:36:56 +0800

    Added string split function

--- PumpItUpEngine/PumpItUpEngine/SSCFile.cpp
+++ PumpItUpEngine/PumpItUpEngine/SSCFile.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <iostream>
 #include <unordered_map>
+#include <regex>
 
 SSCFile::SSCFile(std::string dP, std::string sP) {
 	directoryPath = dP;
@@ -22,20 +23,18 @@ void SSCFile::LoadSSCFileDetails(std::string sscFilePath) {
 
 	bool isHeaders = true;
 	std::string content;
-	int colonIndex = 0;
-	int i = 0;
 	char ch;
 
 	std::unordered_map<std::string, std::string> sscKeyValues;
 
 	while (file.get(ch)) {
-		if (ch == ':') colonIndex = i;
 
 		if (ch == ';') {
 			content.erase(std::remove(content.begin(), content.end(), '\n'), content.end());
 
-			std::string key = content.substr(0, colonIndex);
-			std::string value = content.substr(colonIndex);
+			std::vector<std::string> keyValuePair =  Split(content, ":");
+			std::string key = keyValuePair[0];
+			std::string value = keyValuePair.size() > 1 ? keyValuePair[1]:"";
 
 			if (key == "#NOTEDATA")
 				if (isHeaders) {
@@ -47,14 +46,12 @@ void SSCFile::LoadSSCFileDetails(std::string sscFilePath) {
 				}
 			sscKeyValues[key] = value;
 			content = "";
-			i = 0;
 
 			//std::cout << "Key: " << key << " Value: " << sscKeyValues[key] << std::endl;
 		}
 		else {
 			content += ch;
 		}
-		i++;
 	}
 
 	file.close();
@@ -77,4 +74,17 @@ std::string SSCFile::CombinePaths(const std::string& path1, const std::string& p
 	else {
 		return path1 + '/' + path2;
 	}
+}
+
+std::vector<std::string> SSCFile::Split(const std::string& str, const std::string& delimiter) {
+	std::vector<std::string> tokens;
+	std::regex regex(delimiter);
+	std::sregex_token_iterator iter(str.begin(), str.end(), regex, -1);
+	std::sregex_token_iterator end;
+
+	while (iter != end) {
+		tokens.push_back(*iter++);
+	}
+
+	return tokens;
 }
--- PumpItUpEngine/PumpItUpEngine/SSCFile.h
+++ PumpItUpEngine/PumpItUpEngine/SSCFile.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <string>
+#include <vector>
 class SSCFile
 {
 public:
@@ -10,6 +11,7 @@ public:
 
 private:
 	std::string CombinePaths(const std::string& path1, const std::string& path2);
+	std::vector<std::string> Split(const std::string& str, const std::string& delimiter);
 	std::string directoryPath;
 	std::string sscFile;
 };
