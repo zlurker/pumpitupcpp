2d0b174cbb2de0cea0dd5bae8eaa02c660eada42
Gabriel Chia <gabrieldf_chia@certisgroup.com>
2024-08-03 10:44:56 +0800

    Added texture manager

--- PumpItUpEngine/PumpItUpEngine/TextureManager.cpp
+++ PumpItUpEngine/PumpItUpEngine/TextureManager.cpp
@@ -1,1 +1,17 @@
 #include "TextureManager.h"
+#include <iostream>
+
+void TextureManager::AddTexture(const std::string& tN, const fs::path& fP) {
+	sf::Texture texture;
+	if (!texture.loadFromFile(fP.string())) {
+		std::cerr << "Failed to load image" << std::endl;
+		return;
+	}
+
+	TextureData* textureData = new TextureData(&texture);
+	AddTexture(tN, *textureData);
+}
+
+void TextureManager::AddTexture(const std::string& tN, const TextureData& t) {
+	textures[tN] = t;
+}
--- PumpItUpEngine/PumpItUpEngine/TextureManager.h
+++ PumpItUpEngine/PumpItUpEngine/TextureManager.h
@@ -1,6 +1,15 @@
 #pragma once
+#include "TextureData.h"
+#include <unordered_map>
+#include <filesystem>
+namespace fs = std::filesystem;
+
 class TextureManager
 {
-
+public:
+	void AddTexture(const std::string&, const fs::path&);
+	void AddTexture(const std::string&, const TextureData&);
+private:
+	std::unordered_map<std::string, TextureData> textures;
 };
 
