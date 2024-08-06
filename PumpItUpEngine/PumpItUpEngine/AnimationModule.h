b558b1ab06d077192ada161f40c4cff0639b881c
Gabriel Chia <gabrieldf_chia@certisgroup.com>
2024-08-06 10:58:40 +0800

    Created a class for percentage rect

---
+++ PumpItUpEngine/PumpItUpEngine/AnimationModule.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <vector>
+#include "AnimationTarget.h"
+
+class AnimationModule
+{
+public:
+private:
+	std::vector<AnimationTarget> targets;
+
+};
+
---
+++ PumpItUpEngine/PumpItUpEngine/AnimationTarget.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <string>
+#include "Object.h"
+class AnimationTarget
+{
+public:
+private:
+	Object* targetObject;
+	std::string animationName;
+};
+
